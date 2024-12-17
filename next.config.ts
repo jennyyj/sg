368b906a1ca0e7d8e61ef64604e553c6e31bf725
jennyyj <167258392+jennyyj@users.noreply.github.com>
2024-12-17 23:52:31 +0000

    u

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,12 @@ import withPWA from 'next-pwa';
 const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = withPWA({
-  dest: 'public', // Folder for generated service worker
-  register: true, // Auto-register the service worker
-  skipWaiting: true, // Immediately activate updated service worker
-  disable: !isProduction, // Disable PWA in non-production environments
+  pwa: {
+    dest: 'public', // Folder for generated service worker
+    register: true, // Auto-register the service worker
+    skipWaiting: true, // Immediately activate updated service worker
+    disable: !isProduction, // Disable PWA in non-production environments
+  },
 });
 
 export default nextConfig;
