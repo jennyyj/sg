c2a392b5e4ddb9f76f484ff481d43cc1728cabf5
jennyyj <167258392+jennyyj@users.noreply.github.com>
2024-12-17 20:16:35 +0000

    Ignore TypeScript and ESLint errors during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Enable production builds to ignore TypeScript errors
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Enable production builds to ignore ESLint errors
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
